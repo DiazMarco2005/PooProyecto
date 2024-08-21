1782433cd8db48a88d3d8a02cd50ae6c173a08c5
angc <angcoder.c@gmail.com>
2024-08-21 07:04:51 -0600

    first endpoint

--- shc-server/src/main/java/com/shc/shc_server/controller/ActivityController.java
+++ shc-server/src/main/java/com/shc/shc_server/controller/ActivityController.java
@@ -1,4 +1,23 @@
 package com.shc.shc_server.controller;
 
+import com.shc.shc_server.model.Activity;
+import com.shc.shc_server.service.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
 public class ActivityController {
+    
+    @Autowired
+    private ActivityService activityService;
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return new ResponseEntity<>("pong", HttpStatus.OK);
+    }
 }
