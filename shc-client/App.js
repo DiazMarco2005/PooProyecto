bada1dfd977c2c8c24487a9a34c16d84230ff935
DiazMarco2005 <dia24229@uvg.edu.gt>
2024-08-25 17:44:13 -0600

    Header CSS almost finished

--- shc-client/App.js
+++ shc-client/App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import MyCalendar from "./components/calendar.js";
+import AppHeader from "./components/header.js";
+
 
 export default function App() {
   return (
     <ScrollView>
+      <AppHeader title="Menu" titleStyle={{ color: 'Black' }} headerStyle={{ backgroundColor: '#D9D9D9' }}></AppHeader>
       <View style={styles.container}>
         <MyCalendar start="10:30:00" duration="10:00" description="hola"></MyCalendar>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
       </View>
+
     </ScrollView>
   );
 }
