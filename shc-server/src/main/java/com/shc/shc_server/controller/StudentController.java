a676d6deb054a9ba40d2d026c1b38e753f764f2a
Junjey123-mx <verneljosue@gmail.com>
2024-09-01 19:24:45 -0600

    Update StudentController.java
    Realice el endpoint de verificar horas beca en el controlador de "StudentController".

--- shc-server/src/main/java/com/shc/shc_server/controller/StudentController.java
+++ shc-server/src/main/java/com/shc/shc_server/controller/StudentController.java
@@ -1,4 +1,30 @@
 package com.shc.shc_server.controller;
 
+import com.shc.shc_server.model.Student;
+import com.shc.shc_server.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class StudentController {
+
+    @Autowired
+    private StudentService StudentService;
+
+    @GetMapping("/verifyBecaHours/{id}")
+    public ResponseEntity<Double> verifyBecaHours(@PathVariable Long id){
+        Student student = StudentService.getStudentById(id);
+        if (student != null){
+            Double completedHours = student.getCompletedScholarshipHours();
+            return new ResponseEntity<>(completedHours , HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
 }
