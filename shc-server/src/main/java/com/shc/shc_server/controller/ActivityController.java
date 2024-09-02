b27d4f350711964f05142d61afef6141c73979ba
Junjey123-mx <verneljosue@gmail.com>
2024-09-01 21:10:43 -0600

    Update ActivityController.java
    Agregue el endpoint de "addStudent" en el controlador "ActivityController".

--- shc-server/src/main/java/com/shc/shc_server/controller/ActivityController.java
+++ shc-server/src/main/java/com/shc/shc_server/controller/ActivityController.java
@@ -1,6 +1,7 @@
 package com.shc.shc_server.controller;
 
 import com.shc.shc_server.model.Activity;
+import com.shc.shc_server.model.Student;
 import com.shc.shc_server.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,10 +15,21 @@ import java.util.List;
 public class ActivityController {
     
     @Autowired
-    private ActivityService activityService;
+    private ActivityService ActivityService;
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return new ResponseEntity<>("pong", HttpStatus.OK);
     }
+    
+
+    @PostMapping("{activityId}")
+    public ResponseEntity<Activity> addStudentToActivity(@PathVariable Long activityId, @RequestBody Student student) {
+        Activity activity = ActivityService.getActivityById(activityId);
+        activity.getStudents().add(student);
+        student.setActivity(activity);
+        Activity updatedActivity = ActivityService.saveActivity(activity);
+        return new ResponseEntity<>(updatedActivity, HttpStatus.OK);
+    }
+
 }
