08470e1bce4e07aef454daca2174b005a4d00c79
Junjey123-mx <verneljosue@gmail.com>
2024-09-22 22:15:44 -0600

    Página web idea no terminada.
    Intento básico para página web. 22/09/2024

---
+++ Navegador/main.js
@@ -0,0 +1,10 @@
+let listElements = document.querySelectorAll('.list_button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+        let height = 0;
+        let menu = listElements
+    })
+});
